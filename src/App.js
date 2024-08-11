67c26882e6153c4c3eda1482c292207f30694bab
Jay <apple@apples-MacBook-Pro.local>
2024-08-11 13:02:02 +0530

    jai shree ram

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jai shree ram
         </a>
       </header>
     </div>
